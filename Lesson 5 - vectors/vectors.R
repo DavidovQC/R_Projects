86b80e048085e823d0fb89d075151b0afcdc42f1
Nathan Davidov <NathanDavidov9@gmail.com>
2025-04-27 16:46:33 -0400

    slight edit on Lesson 5

--- Lesson 5 - vectors/vectors.R
+++ Lesson 5 - vectors/vectors.R
@@ -1,2 +1,28 @@
 x <- c(1, 2, 7)
 print(x)
+
+y <- c('R', 'is', 'fun')
+print(y)
+
+z <- c(x, y)
+print(z)
+
+random_data <- runif(6)
+print(random_data)
+
+print(random_data[1])
+print(random_data[100])
+
+subset1 <- random_data[2:5]
+print(subset1)
+
+print(length(subset1))
+
+logical_over_half <- (random_data > 0.5)
+print(logical_over_half)
+over_half <- random_data[logical_over_half]
+print(over_half)
+
+my_letters <- c("a", "b", "c", "d", "f", "a", "a", "c")
+print(my_letters)
+print(my_letters[my_letters %in% c("a", "c")])
