e971bfdb72bc6717050f11cc20e15a3d65f5db14
Nathan Davidov <NathanDavidov9@gmail.com>
2025-05-02 15:04:27 -0400

    Lesson 6 continued

--- Lesson 6 - matrices/matrices.R
+++ Lesson 6 - matrices/matrices.R
@@ -1,2 +1,14 @@
 X <- matrix(data = c(1, 2, 3, 4, 5, 6), nrow = 3, ncol = 2, byrow = FALSE)
 print(X)
+
+X <- matrix(data = c(1, 2, 3, 4, 5, 6), nrow = 2, ncol = 3, byrow = FALSE)
+print(X)
+
+X <- rbind(X, c(7, 8, 9))
+print(X)
+
+X <- cbind(X, c(1, 1, 1))
+print(X)
+
+Y <- rbind(c(1, 2, 3), c(4, 5, 6), c(7, 8, 9))
+print(Y)
